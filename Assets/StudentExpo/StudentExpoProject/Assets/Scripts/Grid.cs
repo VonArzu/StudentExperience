91b757f1e72643f482102b0cb76645f399ff1506
Von Arzu <vonarzu@gmail.com>
2018-12-06 18:29:07 -0500

    School Assets W/Obama

--- Assets/StudentExpo/StudentExpoProject/Assets/Scripts/Grid.cs
+++ Assets/StudentExpo/StudentExpoProject/Assets/Scripts/Grid.cs
@@ -1,10 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Grid : MonoBehaviour {
+    public enum PieceType
+    {
+        NORMAL,
+        COUNT,
+    }
+    [System.Serializable]
 
-	// Use this for initialization
-	void Start () {
+    public struct PiecePrefab
+    {
+        public PieceType type;
+        public GameObject prefab;
+    }
+
+    public int xDim;
+    public int yDim;
+
+    public PiecePrefab[] piecePrefabs;
+
+    private Dictionary< PieceType, GameObject> piecePrefabDict;
+
+
+    void Start () {
 	
 	}
 	
